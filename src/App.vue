a2d5c9e9e5b805eff1c7e0e71e282896d125a399
em2046 <em2046@qq.com>
2020-05-18 19:44:55 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <NovaDatePicker v-model="date"></NovaDatePicker>
+      &nbsp;
+      <NovaAlert type="info" border>First Component</NovaAlert>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      date: null
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'nova-vue/dist/nova.css';
+import { NovaDatePicker, NovaAlert } from 'nova-vue';
+
+Vue.component('NovaAlert', NovaAlert);
+Vue.component('NovaDatePicker', NovaDatePicker);
+
 Vue.config.productionTip = false
 
 new Vue({
